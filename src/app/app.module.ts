14812c659fa86463f00fcd629a199378549fd1b8
Bruno Chirelli <brunochirelli@gmail.com>
2023-03-15 10:27:15 +0000

    add state service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RpdFormComponent } from './components/rpd-form/rpd-form.component';
+import { RpdListComponent } from './components/rpd-list/rpd-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RpdFormComponent,
+    RpdListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/service/state.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Event } from '../types/event.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StateService {
+  rpd: Event[] = [
+    {
+      id: 1,
+      event: 'Nome do Evento',
+      date: '2021-01-01',
+      emotions: [
+        {
+          id: 1,
+          name: 'Feliz',
+          percentage: 0.5,
+        },
+        {
+          id: 2,
+          name: 'Triste',
+          percentage: 0.3,
+        },
+      ],
+      reaction: 'Reação automática',
+      rule: 'Regra',
+    },
+  ];
+
+  constructor() {}
+}
